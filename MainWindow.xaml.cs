cad9c5cecf39f555c78e62afaa7b2aa94bd3a41b
Zac <zac@zacland.net>
2020-02-27 09:13:05 +0100

    init

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Data;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SQLiteApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<PersonModel> people = new List<PersonModel>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            LoadPeopleList();
+            SQLiteDataAccess.LoadGridPeople(PeopleGrid);
+        }
+
+        private void LoadPeopleList()
+        {
+            // TODO - Get real data here
+            people = SQLiteDataAccess.LoadPeople();
+
+            WireUpPeopleList();
+        }
+
+        private void WireUpPeopleList()
+        {
+            listPeopleListBox.ItemsSource = null;
+            listPeopleListBox.ItemsSource = people;
+            listPeopleListBox.DisplayMemberPath = "FullName";
+        }
+
+        private void refreshListButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoadPeopleList();
+            SQLiteDataAccess.LoadGridPeople(PeopleGrid);
+        }
+
+        private void addPersonButton_Click(object sender, RoutedEventArgs e)
+        {
+            PersonModel p = new PersonModel();
+
+            p.FirstName = firstNameText.Text;
+            p.LastName = lastNameText.Text;
+
+            SQLiteDataAccess.SavePerson(p);
+
+            firstNameText.Text = "";
+            lastNameText.Text = "";
+        }
+    }
+}
---
+++ SQLiteDataAccess.cs
@@ -0,0 +1,58 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace SQLiteApplication
+{
+    public class SQLiteDataAccess
+    {
+        public static List<PersonModel> LoadPeople()
+        {
+            using(IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                var output = cnn.Query<PersonModel>("SELECT * FROM Person", new DynamicParameters());
+                return output.ToList();
+            }
+        }
+
+        public static void LoadGridPeople(DataGrid dg) 
+        {
+            SQLiteConnection cnn = new SQLiteConnection(LoadConnectionString());
+
+            cnn.Open();
+
+            string Query = "SELECT * FROM Person";
+
+            SQLiteCommand createCommand = new SQLiteCommand(Query, cnn);
+            createCommand.ExecuteNonQuery();
+
+
+            SQLiteDataAdapter dataAdapt = new SQLiteDataAdapter(createCommand);
+            DataTable dt = new DataTable("Person");
+            dataAdapt.Fill(dt);
+            dg.ItemsSource = dt.DefaultView;
+            dataAdapt.Update(dt);
+            cnn.Close();
+        }
+
+        public static void SavePerson (PersonModel person)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                cnn.Execute("INSERT INTO Person (FirstName, LastName) values (@FirstName, @LastName)", person);
+            }
+        }
+
+        private static string LoadConnectionString(string id = "Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
